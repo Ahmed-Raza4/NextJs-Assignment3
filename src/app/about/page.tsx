14fbf612e164e37687afcbde6f966499803fbb20
Ahmed-Raza4 <hafizahmedraza12345@gmail.com>
2024-09-29 20:41:11 +0500

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+
+export default function About(){
+    return(
+        <div>
+        <Header/> 
+        <div className="about">
+            <h1>About Page</h1>
+            <p>This is the about page.</p>
+        </div>
+        </div>
+    )
+}
+
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header"
+
+export default function Contact(){
+    return(
+        <div>
+            <Header/>
+        <div className="contact">
+            <h1>Contact Us</h1>
+            <p>Contact us at 0313-0221118</p>
+        </div>
+        </div>
+    )
+}
